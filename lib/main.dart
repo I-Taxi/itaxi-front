e61e64f4d435c3113a503eef7b537445dee863f0
안지원 <anjiwon@anjiwon-ui-MacBookPro.local>
2022-07-07 11:19:34 +0900

    feat: Add routes

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:itaxi/controller/signInController.dart';
+import 'package:itaxi/mainScreen.dart';
+import 'package:itaxi/settings/settingScreen.dart';
 import 'package:itaxi/signInUp/signInScreen.dart';
+import 'package:itaxi/signInUp/signUpScreen.dart';
 import 'package:itaxi/src/theme.dart';
+import 'package:itaxi/timeline/timelineScreen.dart';
 
 void main() {
   // firebase_core 패키지 필요
@@ -21,17 +25,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        title: 'iTaxi',
-        theme: ITaxiTheme.lightThemeData,
-        home: GetBuilder<SignInController>(
-          builder: (_) {
-            return SignInScreen();
-            // if (_signInController.signInState == SignInState.signedOut) {
-            //   return SignInScreen();
-            // } else {
-            //   return MainScreen();
-            // }
-          },
-        ));
+      title: 'iTaxi',
+      theme: ITaxiTheme.lightThemeData,
+      home: GetBuilder<SignInController>(
+        builder: (_) {
+          return SignInScreen();
+          // if (_signInController.signInState == SignInState.signedOut) {
+          //   return SignInScreen();
+          // } else {
+          //   return MainScreen();
+          // }
+        },
+      ),
+      routes: {
+        '/signin': (context) => SignInScreen(),
+        '/signup': (context) => SignUpScreen(),
+        '/': (context) => MainScreen(),
+        '/timeline': (context) => TimelineScreen(),
+        '/settings': (context) => SettingScreen(),
+      },
+    );
   }
 }
