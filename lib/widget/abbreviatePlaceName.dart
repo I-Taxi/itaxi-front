00257e751a6f563b6f61aed575512a6d52e53ae2
Doplin <soeunsense7@gmail.com>
2023-02-07 10:48:04 +0900

    feat[#138]: AbbreviatePlaceName 위젯 제작
    - 장소 약어로 만드는 위젯 제작
    - historyListContainer에 적용

---
+++ lib/widget/abbreviatePlaceName.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+String AbbreviatePlaceName(String? place) {
+  if (place == '그할마(CU장량그랜드점)') {
+    return '그할마';
+  } else if (place == '야옹아 멍멍해봐') {
+    return '야옹멍멍';
+  } else if (place == '포항고속버스터미널') {
+    return '포항고터';
+  } else if (place == '포항시외버스터미널') {
+    return '포항시터';
+  } else if (place == '태화강 식당 사거리') {
+    return '태화강';
+  } else if (place != null) {
+    return place;
+  } else {
+    return 'error';
+  }
+}
