717c76e8ce3900cecab2b989f0685d944444b621
Doplin <soeunsense7@gmail.com>
2023-02-08 13:56:09 +0900

    feat[#154]: timdlineSoonInfoCard 경유지 card 추가

--- lib/timeline/timelineSoonInfoCard.dart
+++ lib/timeline/timelineSoonInfoCard.dart
@@ -21,16 +21,139 @@ Container timelineSoonInfoCard({required BuildContext context, required Post pos
   late PostController _postController = Get.find();
   late HistoryController _historyController = Get.find();
 
+  // if (post.stopovers!.isNotEmpty) {
+  //   return Container(
+  //     width: 339.w,
+  //     height: 230.h,
+  //     decoration: BoxDecoration(borderRadius: BorderRadius.circular(16), color: colorScheme.primary, boxShadow: [
+  //       BoxShadow(
+  //         color: colorScheme.shadow,
+  //         blurRadius: 40,
+  //         offset: const Offset(2, 4),
+  //       )
+  //     ]),
+  //     child: Padding(
+  //         padding: EdgeInsets.fromLTRB(25.w, 18.h, 21.w, 0.h),
+  //         child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+  //           Text(
+  //             '곧 출발 예정',
+  //             style: textTheme.subtitle2?.copyWith(color: colorScheme.tertiaryContainer),
+  //           ),
+  //           SizedBox(
+  //             height: 15.h,
+  //           ),
+  //           Row(
+  //             crossAxisAlignment: CrossAxisAlignment.end,
+  //             children: [
+  //               Image.asset(width: 24, 'assets/icon/location.png'),
+  //               SizedBox(
+  //                 width: 20.w,
+  //               ),
+  //               Text(
+  //                 '${post.departure?.name}',
+  //                 style: textTheme.subtitle2?.copyWith(color: colorScheme.onTertiary),
+  //               ),
+  //               const Spacer(),
+  //             ],
+  //           ),
+  //           SizedBox(
+  //             height: 16.h,
+  //           ),
+  //           Row(
+  //             crossAxisAlignment: CrossAxisAlignment.end,
+  //             children: [
+  //               Image.asset(width: 24, 'assets/icon/location.png'),
+  //               SizedBox(
+  //                 width: 20.w,
+  //               ),
+  //               Text(
+  //                 '${post.destination?.name}',
+  //                 style: textTheme.subtitle2?.copyWith(color: colorScheme.onTertiary),
+  //               ),
+  //               const Spacer(),
+  //             ],
+  //           ),
+  //           SizedBox(
+  //             height: 14.h,
+  //           ),
+  //           HorizontalDashedDivider(
+  //             length: 5,
+  //             thickness: 1,
+  //             indent: 0,
+  //             endIndent: 0,
+  //           ),
+  //           SizedBox(
+  //             height: 18.h,
+  //           ),
+  //           Row(
+  //             children: [
+  //               Column(
+  //                 crossAxisAlignment: CrossAxisAlignment.start,
+  //                 children: [
+  //                   Text(
+  //                     DateFormat('MM/dd').format(DateTime.parse(post.deptTime!)),
+  //                     style: textTheme.subtitle2?.copyWith(color: colorScheme.tertiaryContainer),
+  //                   ),
+  //                   SizedBox(
+  //                     height: 15.h,
+  //                   ),
+  //                   Text(
+  //                     postTypeToString(post.postType),
+  //                     style: textTheme.subtitle2?.copyWith(color: colorScheme.tertiaryContainer),
+  //                   ),
+  //                 ],
+  //               ),
+  //               SizedBox(
+  //                 width: 49.w,
+  //               ),
+  //               Column(
+  //                 crossAxisAlignment: CrossAxisAlignment.start,
+  //                 children: [
+  //                   Text(
+  //                     DateFormat('HH:mm').format(DateTime.parse(post.deptTime!)),
+  //                     style: textTheme.subtitle2?.copyWith(color: colorScheme.tertiaryContainer),
+  //                   ),
+  //                   SizedBox(
+  //                     height: 15.h,
+  //                   ),
+  //                   Text(
+  //                     '${post.participantNum}/${post.capacity}명',
+  //                     style: textTheme.subtitle2?.copyWith(color: colorScheme.tertiaryContainer),
+  //                   ),
+  //                 ],
+  //               ),
+  //               const Spacer(),
+  //               GestureDetector(
+  //                   onTap: () async {
+  //                     if (post.postType == null) {
+  //                       // TODO: ktx container 제작하면 연결
+  //                     } else {
+  //                       _historyController.getHistoryInfo(postId: post.id!);
+  //                       Get.to(() => const TimelineDetailScreen());
+  //                     }
+  //                   },
+  //                   child: Image.asset(width: 81.w, 'assets/button/go_descript.png'))
+  //             ],
+  //           ),
+  //           SizedBox(
+  //             height: 16.h,
+  //           ),
+  //           const Spacer(),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // } else {
   return Container(
-    width: 339.w,
-    height: 230.h,
-    decoration: BoxDecoration(borderRadius: BorderRadius.circular(16), color: colorScheme.primary, boxShadow: [
-      BoxShadow(
-        color: colorScheme.shadow,
-        blurRadius: 40,
-        offset: const Offset(2, 4),
-      )
-    ]),
+      width: 339.w,
+      height: 268.h,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(16), color: colorScheme.primary, boxShadow: [
+        BoxShadow(
+          color: colorScheme.shadow,
+          blurRadius: 40,
+          offset: const Offset(2, 4),
+        )
+      ]),
       child: Padding(
           padding: EdgeInsets.fromLTRB(25.w, 18.h, 21.w, 0.h),
           child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
@@ -52,6 +175,23 @@ Container timelineSoonInfoCard({required BuildContext context, required Post pos
                   '${post.departure?.name}',
                   style: textTheme.subtitle2?.copyWith(color: colorScheme.onTertiary),
                 ),
+                const Spacer(),
+              ],
+            ),
+            SizedBox(
+              height: 16.h,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                SizedBox(
+                  width: 20.w,
+                ),
+                if (post.stopovers != null)
+                  Text(
+                    '${post.stopovers![0]?.name}',
+                    style: textTheme.subtitle2?.copyWith(color: colorScheme.onTertiary),
+                  ),
                 const Spacer(),
               ],
             ),
@@ -138,6 +278,6 @@ Container timelineSoonInfoCard({required BuildContext context, required Post pos
               height: 16.h,
             ),
             const Spacer(),
-          ]))
-  );
+          ])));
+  // }
 }
