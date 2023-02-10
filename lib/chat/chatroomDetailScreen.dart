2124332044cd8e3d6006479c0155682a67a1d951
Hyeongjin Kim <hhjj0506@gmail.com>
2023-02-10 21:39:57 +0900

    fix[#212]: join message UI edited

--- lib/chat/chatroomDetailScreen.dart
+++ lib/chat/chatroomDetailScreen.dart
@@ -542,18 +542,56 @@ class _ChatRoomDetailScreenState extends State<ChatRoomDetailScreen> {
                                               ],
                                             ),
                                         snapshot.data![index].memberName == null
-                                            ? Padding(
-                                                padding: EdgeInsets.symmetric(
-                                                  vertical: 6.h,
-                                                ),
-                                                child: Text(
-                                                  snapshot
-                                                      .data![index].chatData!,
-                                                  style: textTheme.bodyText1
-                                                      ?.copyWith(
-                                                    color: colorScheme.tertiary,
-                                                  ),
-                                                ),
+                                            ? Column(
+                                                children: [
+                                                  index > 0 &&
+                                                          snapshot
+                                                                  .data![
+                                                                      index - 1]
+                                                                  .memberName ==
+                                                              null
+                                                      ? SizedBox()
+                                                      : SizedBox(
+                                                          height: 21.h,
+                                                        ),
+                                                  Container(
+                                                      decoration: BoxDecoration(
+                                                          color: colorScheme
+                                                              .onBackground,
+                                                          borderRadius:
+                                                              const BorderRadius
+                                                                      .all(
+                                                                  Radius
+                                                                      .circular(
+                                                                          5))),
+                                                      child: Padding(
+                                                        padding: EdgeInsets
+                                                            .symmetric(
+                                                                vertical: 6.h,
+                                                                horizontal:
+                                                                    8.w),
+                                                        child: Text(
+                                                          snapshot.data![index]
+                                                              .chatData!,
+                                                          style: textTheme
+                                                              .bodyText2
+                                                              ?.copyWith(
+                                                            color: colorScheme
+                                                                .tertiaryContainer,
+                                                          ),
+                                                        ),
+                                                      )),
+                                                  index > 0 &&
+                                                          snapshot
+                                                                  .data![
+                                                                      index - 1]
+                                                                  .memberName ==
+                                                              null
+                                                      ? SizedBox()
+                                                      : SizedBox(
+                                                          height: 17.h,
+                                                        )
+                                                ],
                                               )
                                             : ChatDetailListTile(
                                                 context: context,
