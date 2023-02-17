9015150bbfb09a5e8e3b0610f5292552505b6125
zan1206 <22100752@handong.ac.kr>
2023-02-17 23:48:38 +0900

    fix[#251]: 햄버거바 수정, 동료 구하기 활성화

--- lib/timeline/ktxCheckPlaceScreen.dart
+++ lib/timeline/ktxCheckPlaceScreen.dart
@@ -8,11 +8,23 @@ import 'package:itaxi/controller/dateController.dart';
 import 'package:itaxi/controller/ktxPlaceController.dart';
 import 'package:itaxi/controller/ktxPostController.dart';
 import 'package:itaxi/controller/screenController.dart';
+import 'package:itaxi/mainScreen.dart';
 import 'package:itaxi/model/ktxPost.dart';
 import 'package:itaxi/widget/ktxPostListTile.dart';
 import 'package:itaxi/widget/abbreviatePlaceName.dart';
 
 import 'package:itaxi/controller/userController.dart';
+import 'package:itaxi/controller/navigationController.dart';
+import 'package:itaxi/controller/signInController.dart';
+import 'package:itaxi/model/userInfoList.dart';
+import 'package:itaxi/settings/alarmScreen.dart';
+import 'package:itaxi/settings/bugScreen.dart';
+import 'package:itaxi/settings/myInfoScreen.dart';
+import 'package:itaxi/settings/noticeScreen.dart';
+import 'package:itaxi/settings/privacyPolicyScreen.dart';
+import 'package:itaxi/settings/termOfServiceScreen.dart';
+import 'package:itaxi/settings/versionScreen.dart';
+import 'package:flutter/cupertino.dart';
 
 class KtxCheckPlaceScreen extends StatefulWidget {
   const KtxCheckPlaceScreen({Key? key}) : super(key: key);
@@ -28,8 +40,19 @@ class _KtxCheckPlaceScreenState extends State<KtxCheckPlaceScreen> {
   KtxPlaceController _ktxPlaceController = Get.put(KtxPlaceController());
   DateController _dateController = Get.put(DateController());
   UserController _userController = Get.put(UserController());
+  NavigationController _navController = Get.put(NavigationController());
+  SignInController _signInController = Get.put(SignInController());
+
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
       GlobalKey<RefreshIndicatorState>();
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  bool isOpen = false;
+
+  void _openEndDrawer() {
+    _scaffoldKey.currentState!.openEndDrawer();
+  }
 
   @override
   void initState() {
@@ -73,6 +96,11 @@ class _KtxCheckPlaceScreenState extends State<KtxCheckPlaceScreen> {
           ),
         ),
         Scaffold(
+          key: _scaffoldKey,
+          endDrawer: Drawer(child: _myListView(context: context)),
+          onEndDrawerChanged: (isOpen) {
+            _navController.changeNavHeight(isOpen);
+          },
           appBar: AppBar(
             backgroundColor: Colors.transparent,
             elevation: 0.0,
@@ -86,7 +114,9 @@ class _KtxCheckPlaceScreenState extends State<KtxCheckPlaceScreen> {
               Padding(
                 padding: EdgeInsets.only(right: 12.r),
                 child: IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    _openEndDrawer();
+                  },
                   icon: Image.asset(
                     "assets/button/menu.png",
                     color: colorScheme.background,
@@ -322,7 +352,10 @@ class _KtxCheckPlaceScreenState extends State<KtxCheckPlaceScreen> {
               height: 18.h,
             ),
             OutlinedButton(
-              onPressed: () {},
+              onPressed: () {
+                _screenController.changeKtxScreenToggleIndex(1);
+                _screenController.setKtxCheckScreen(false);
+              },
               style: OutlinedButton.styleFrom(
                   side:
                       BorderSide(width: 0.01, color: colorScheme.onBackground)),
@@ -336,5 +369,298 @@ class _KtxCheckPlaceScreenState extends State<KtxCheckPlaceScreen> {
         )
       ],
     );
+  }
+
+  Widget _myListView({required BuildContext context}) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+    return GetBuilder<UserController>(builder: (_) {
+      return FutureBuilder<UserInfoList>(
+          future: _userController.users,
+          builder: (BuildContext context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 87.h, left: 35.w, right: 24.w),
+                    child: Image(
+                      image: AssetImage("assets/profile.png"),
+                      height: 88.w,
+                      width: 88.w,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 22.h,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 35.w, right: 24.w),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "${snapshot.data!.name.toString()}학부생",
+                              style: textTheme.headline3?.copyWith(
+                                color: colorScheme.onPrimary,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 6.h,
+                            ),
+                            Text(
+                              snapshot.data!.email.toString(),
+                              style: textTheme.bodyText2?.copyWith(color: colorScheme.tertiaryContainer, fontSize: 10.sp),
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        IconButton(
+                          onPressed: () {
+                            Get.to(MyInfoScreen());
+                          },
+                          icon: Icon(
+                            Icons.arrow_forward_ios,
+                            size: 16.h,
+                          ),
+                          color: colorScheme.tertiaryContainer,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16.h,
+                  ),
+                  Container(
+                    height: 1.5.h,
+                    width: 390.w,
+                    color: Color(0xE1E1E1E1),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 35.w, right: 24.w),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 28.h,
+                        ),
+                        _settingListTile(
+                          title: '공지사항',
+                          nextPage: NoticeScreen(),
+                          context: context,
+                        ),
+                        SizedBox(
+                          height: 32.h,
+                        ),
+                        _alarmListTile(
+                          title: '알림',
+                          nextPage: const AlarmScreen(),
+                          context: context,
+                        ),
+                        SizedBox(
+                          height: 32.h,
+                        ),
+                        _settingListTile(
+                          title: '버그제보',
+                          nextPage: BugScreen(),
+                          context: context,
+                        ),
+                        SizedBox(
+                          height: 32.h,
+                        ),
+                        _settingListTile(
+                          title: '약관',
+                          nextPage: const TermOfServiceScreen(),
+                          context: context,
+                        ),
+                        SizedBox(
+                          height: 32.h,
+                        ),
+                        _settingListTile(
+                          title: '버전정보 / 개발자',
+                          nextPage: const VersionScreen(),
+                          context: context,
+                        ),
+                        SizedBox(
+                          height: 32.h,
+                        ),
+                        _settingListTile(
+                          title: '개인정보처리방침',
+                          nextPage: const PrivacyPolicyScreen(),
+                          context: context,
+                        ),
+                        SizedBox(
+                          height: 205.h,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Created by CRA",
+                              style: textTheme.bodyText2?.copyWith(
+                                color: colorScheme.tertiary,
+                              ),
+                            ),
+                            Spacer(),
+                            IconButton(
+                              onPressed: () {
+                                _logout(context: context);
+                              },
+                              icon: Image.asset("assets/logout.png"),
+                              color: colorScheme.tertiary,
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                _logout(context: context);
+                              },
+                              child: Text(
+                                "로그아웃",
+                                style: textTheme.bodyText2?.copyWith(color: colorScheme.tertiaryContainer),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              );
+            } else {
+              return Container(
+                child: GestureDetector(
+                  onTap: () {
+                    _logout(context: context);
+                  },
+                  child: Text(
+                    "로그아웃",
+                    style: textTheme.bodyText2?.copyWith(color: colorScheme.tertiary),
+                  ),
+                ),
+              );
+            }
+          });
+    });
+  }
+
+  // 알림 타일
+  Widget _alarmListTile({
+    required String title,
+    required nextPage,
+    required BuildContext context,
+  }) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: () {
+            Get.to(nextPage);
+          },
+          child: Text(
+            title,
+            textAlign: TextAlign.start,
+            style: textTheme.subtitle1?.copyWith(color: colorScheme.onTertiary),
+          ),
+        ),
+        Spacer(),
+        Container(
+          height: 17.h,
+          width: 34.w,
+          child: Transform.scale(
+            transformHitTests: false,
+            scale: .6,
+            child: CupertinoSwitch(
+                value: _userController.alarm,
+                activeColor: colorScheme.inverseSurface,
+                thumbColor: colorScheme.primary,
+                trackColor: colorScheme.tertiary,
+                onChanged: (bool value) {
+                  _userController.setAlarm(value);
+                }),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _settingListTile({
+    required String title,
+    required nextPage,
+    required BuildContext context,
+  }) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+    return GestureDetector(
+      onTap: () {
+        Get.to(nextPage);
+      },
+      child: Text(
+        title,
+        textAlign: TextAlign.start,
+        style: textTheme.subtitle1?.copyWith(color: colorScheme.onTertiary),
+      ),
+    );
+  }
+
+  void _logout({
+    required BuildContext context,
+  }) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            elevation: 0,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+            child: Container(
+              width: 360.w,
+              height: 240.h,
+              padding: EdgeInsets.fromLTRB(
+                36.0.w,
+                50.0.h,
+                36.0.w,
+                50.0.h,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    "로그아웃 하시겠습니까?",
+                    style: textTheme.subtitle1?.copyWith(color: colorScheme.onSecondaryContainer),
+                  ),
+                  const Spacer(),
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    TextButton(
+                        onPressed: () => Navigator.pop(context, "취소"),
+                        child: Text(
+                          "취소",
+                          style: textTheme.subtitle1?.copyWith(
+                            color: colorScheme.tertiaryContainer,
+                          ),
+                        )),
+                    SizedBox(
+                      width: 60.w,
+                    ),
+                    TextButton(
+                        onPressed: () async {
+                          _signInController.reset();
+                          _signInController.signedOutState();
+                          _navController.changeIndex(0);
+                          Navigator.pop(context);
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          "확인",
+                          style: textTheme.subtitle1?.copyWith(
+                            color: colorScheme.onSecondaryContainer,
+                          ),
+                        )),
+                  ])
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
